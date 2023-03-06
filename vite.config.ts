ff72a9f08ceb697fa3ebeea3272d0063a91bdeb0
Matsura Yuma <41217985+sabigara@users.noreply.github.com>
2023-03-06 11:38:42 +0900

    Support pwa

--- vite.config.ts
+++ vite.config.ts
@@ -2,12 +2,39 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import tsconfigPaths from "vite-tsconfig-paths";
 import svgr from "vite-plugin-svgr";
+import { VitePWA } from "vite-plugin-pwa";
 
 import { generateScopedName, hash } from "@camome/utils";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [tsconfigPaths(), react(), svgr({ exportAsDefault: true })],
+  plugins: [
+    tsconfigPaths(),
+    react(),
+    svgr({ exportAsDefault: true }),
+    VitePWA({
+      registerType: "autoUpdate",
+      includeAssets: ["favicon.ico", "apple-touch-icon.png"],
+      manifest: {
+        name: "Flat",
+        short_name: "Flat",
+        description: "A Bluesky client",
+        theme_color: "#2563eb",
+        icons: [
+          {
+            src: "icon-192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "icon-512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
+      },
+    }),
+  ],
   css: {
     modules: {
       generateScopedName(name, filename) {
