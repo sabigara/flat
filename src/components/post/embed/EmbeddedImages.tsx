4b5f036cdbbcd3f8babbb2979a543c204eb6ec81
Matsura Yuma <41217985+sabigara@users.noreply.github.com>
2023-03-10 22:07:16 +0900

    Add lightbox

--- src/components/post/embed/EmbeddedImages.tsx
+++ src/components/post/embed/EmbeddedImages.tsx
@@ -1,6 +1,9 @@
 import { AppBskyEmbedImages } from "@atproto/api";
 import clsx from "clsx";
+import React from "react";
+import Lightbox from "yet-another-react-lightbox";
 
+import "yet-another-react-lightbox/styles.css";
 import styles from "./EmbeddedImages.module.scss";
 
 type Props = {
@@ -9,25 +12,44 @@ type Props = {
 };
 
 export default function EmbeddedImages({ embed, className }: Props) {
+  const [open, setOpen] = React.useState(false);
   return (
-    <div
-      className={clsx(styles.container, className, {
-        [styles.two]: embed.images.length === 2,
-        [styles.three]: embed.images.length === 3,
-        [styles.four]: embed.images.length === 4,
-      })}
-    >
-      {embed.images.map((img) => (
-        <a
-          key={img.thumb}
-          href={img.fullsize}
-          target="_blank"
-          rel="noopener noreferrer"
-          onClick={(e) => e.stopPropagation()}
-        >
-          <img src={img.thumb} alt={img.alt} />
-        </a>
-      ))}
-    </div>
+    <>
+      <div
+        className={clsx(styles.container, className, {
+          [styles.two]: embed.images.length === 2,
+          [styles.three]: embed.images.length === 3,
+          [styles.four]: embed.images.length === 4,
+        })}
+      >
+        {embed.images.map((img) => (
+          <button
+            key={img.thumb}
+            onClick={(e) => {
+              e.stopPropagation();
+              setOpen(true);
+            }}
+          >
+            <img src={img.thumb} alt={img.alt} />
+          </button>
+        ))}
+      </div>
+      <Lightbox
+        open={open}
+        close={() => setOpen(false)}
+        slides={embed.images.map((img) => ({
+          src: img.fullsize,
+        }))}
+        animation={{
+          navigation: {
+            duration: 1,
+            easing: "linear",
+          },
+        }}
+        controller={{ closeOnBackdropClick: true }}
+        carousel={{ finite: true }}
+        className={styles.yarl}
+      />
+    </>
   );
 }
