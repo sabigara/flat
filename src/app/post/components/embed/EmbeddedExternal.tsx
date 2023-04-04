c5988acb8a96ffcd075c87a58cb6f976e2707a86
Matsura Yuma <41217985+sabigara@users.noreply.github.com>
2023-04-04 19:29:46 +0900

    Fix type

--- src/app/post/components/embed/EmbeddedExternal.tsx
+++ src/app/post/components/embed/EmbeddedExternal.tsx
@@ -4,7 +4,7 @@ import clsx from "clsx";
 import styles from "./EmbeddedExternal.module.scss";
 
 type Props = {
-  external: AppBskyEmbedExternal.External;
+  external: AppBskyEmbedExternal.ViewExternal;
   className?: string;
 };
 
