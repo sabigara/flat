6348f6919ede57120fa5c2d6331d96f9a78cffce
Matsura Yuma <41217985+sabigara@users.noreply.github.com>
2023-04-25 10:10:38 +0900

    Remove unused i18next-http-backend

--- src/i18n/config.ts
+++ src/i18n/config.ts
@@ -1,6 +1,5 @@
 import i18next from "i18next";
 import LanguageDetector from "i18next-browser-languagedetector";
-import Backend from "i18next-http-backend";
 import { initReactI18next } from "react-i18next";
 
 import enCommon from "./en/common.json";
@@ -29,7 +28,7 @@ export const resources = {
   },
 } as const;
 
-i18next.use(Backend).use(LanguageDetector).use(initReactI18next).init({
+i18next.use(LanguageDetector).use(initReactI18next).init({
   debug: true,
   resources,
   defaultNS,
