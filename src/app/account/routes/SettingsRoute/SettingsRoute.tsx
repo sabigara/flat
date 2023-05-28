e6c57b4b893b7b33091d87435cd4eb42883cf085
Matsura Yuma <41217985+sabigara@users.noreply.github.com>
2023-05-28 21:23:48 +0900

    Move FeedFilter to settings

--- src/app/account/routes/SettingsRoute/SettingsRoute.tsx
+++ src/app/account/routes/SettingsRoute/SettingsRoute.tsx
@@ -1,3 +1,5 @@
+import { Radio } from "@camome/core/Radio";
+import { RadioGroup } from "@camome/core/RadioGroup";
 import { Select } from "@camome/core/Select";
 import { useImmerAtom } from "jotai-immer";
 import { useTranslation } from "react-i18next";
@@ -8,52 +10,102 @@ import type { Theme } from "@/src/app/theme/lib/types";
 
 import { Mode } from "@/src/app/account/lib/types";
 import { settingsAtom } from "@/src/app/account/states/settingsAtom";
+import { FeedFilterReply, FeedFilterRepost } from "@/src/app/feed/lib/types";
 import { InFeedThreadMode, PostImageLayout } from "@/src/app/post/lib/types";
 import Seo from "@/src/app/seo/Seo";
 
 import styles from "./SettingsRoute.module.scss";
 
+type RegisterFeedFilter = (
+  name: "reply" | "repost",
+  value: FeedFilterReply | FeedFilterRepost
+) => {
+  name: string;
+  value: string;
+  checked: boolean;
+  onChange: React.FormEventHandler<HTMLInputElement>;
+};
+
+const RADIO_SIZE = "sm" as const;
+
 export function SettingsRoute() {
-  const { t, i18n } = useTranslation("settings");
+  const { t, i18n } = useTranslation();
+  const { t: settingsT } = useTranslation("settings");
   const [settings, setSettings] = useImmerAtom(settingsAtom);
   const { theme } = useOutletContext<RootContext>();
 
+  const {
+    tlFilters: { reply, repost },
+  } = settings;
+  const register: RegisterFeedFilter = (
+    name: "reply" | "repost",
+    value: FeedFilterReply | FeedFilterRepost
+  ) => {
+    return {
+      name,
+      value,
+      checked: (name === "reply" ? reply : repost) === value,
+      onChange: (e) => {
+        if (e.currentTarget.checked) {
+          if (e.currentTarget.name === "reply") {
+            setSettings((draft) => {
+              draft.tlFilters.reply = e.currentTarget.value as FeedFilterReply;
+            });
+          } else {
+            setSettings((draft) => {
+              draft.tlFilters.repost = e.currentTarget
+                .value as FeedFilterRepost;
+            });
+          }
+        }
+      },
+    };
+  };
+
   return (
     <>
-      <Seo title={t("title")} />
+      <Seo title={settingsT("title")} />
       <div className={styles.container}>
-        <h1 className={styles.title}>{t("title")}</h1>
+        <h1 className={styles.title}>{settingsT("title")}</h1>
 
         <section className={styles.section}>
-          <h2>{t("appearance.title")}</h2>
+          <h2>{settingsT("appearance.title")}</h2>
 
           <Select
-            label={t("appearance.mode.title")}
+            label={settingsT("appearance.mode.title")}
             size="md"
             value={settings.mode}
             onChange={(e) =>
               setSettings((draft) => void (draft.mode = e.target.value as Mode))
             }
           >
-            <option value="all">{t("appearance.mode.options.all")}</option>
-            <option value="zen">{t("appearance.mode.options.zen")}</option>
-          </Select>
-
-          <Select
-            label={t("appearance.theme.title")}
-            size="md"
-            value={theme.value}
-            onChange={(e) => theme.set(e.target.value as Theme)}
-          >
-            <option value="light">{t("appearance.theme.options.light")}</option>
-            <option value="dark">{t("appearance.theme.options.dark")}</option>
-            <option value="system">
-              {t("appearance.theme.options.system")}
+            <option value="all">
+              {settingsT("appearance.mode.options.all")}
+            </option>
+            <option value="zen">
+              {settingsT("appearance.mode.options.zen")}
             </option>
           </Select>
 
           <Select
-            label={t("appearance.image-layout.title")}
+            label={settingsT("appearance.theme.title")}
+            size="md"
+            value={theme.value}
+            onChange={(e) => theme.set(e.target.value as Theme)}
+          >
+            <option value="light">
+              {settingsT("appearance.theme.options.light")}
+            </option>
+            <option value="dark">
+              {settingsT("appearance.theme.options.dark")}
+            </option>
+            <option value="system">
+              {settingsT("appearance.theme.options.system")}
+            </option>
+          </Select>
+
+          <Select
+            label={settingsT("appearance.image-layout.title")}
             size="md"
             value={settings.postImageLayout}
             onChange={(e) =>
@@ -65,15 +117,15 @@ export function SettingsRoute() {
             }
           >
             <option value="stack">
-              {t("appearance.image-layout.options.stack")}
+              {settingsT("appearance.image-layout.options.stack")}
             </option>
             <option value="compact">
-              {t("appearance.image-layout.options.compact")}
+              {settingsT("appearance.image-layout.options.compact")}
             </option>
           </Select>
 
           <Select
-            label={t("appearance.in-feed-thread.title")}
+            label={settingsT("appearance.in-feed-thread.title")}
             size="md"
             value={settings.inFeedThreadMode}
             onChange={(e) =>
@@ -85,18 +137,20 @@ export function SettingsRoute() {
             }
           >
             <option value="aggregate">
-              {t("appearance.in-feed-thread.options.aggregate")}
+              {settingsT("appearance.in-feed-thread.options.aggregate")}
             </option>
             <option value="reverse-chronological">
-              {t("appearance.in-feed-thread.options.reverse-chronological")}
+              {settingsT(
+                "appearance.in-feed-thread.options.reverse-chronological"
+              )}
             </option>
           </Select>
         </section>
 
         <section className={styles.section}>
-          <h2>{t("language.title")}</h2>
+          <h2>{settingsT("language.title")}</h2>
           <Select
-            label={t("language.title")}
+            label={settingsT("language.title")}
             size="md"
             value={i18n.resolvedLanguage}
             onChange={(e) => i18n.changeLanguage(e.target.value)}
@@ -104,6 +158,53 @@ export function SettingsRoute() {
             <option value="ja">日本語</option>
             <option value="en">English</option>
           </Select>
+        </section>
+
+        {/* FIXME */}
+        <section className={styles.section}>
+          <h2>{settingsT("content-filters.title")}</h2>
+          <RadioGroup
+            label={t("post.reply.title")}
+            aria-required
+            orientation="horizontal"
+          >
+            <Radio
+              label={t("feed.filters.reply.all")}
+              size={RADIO_SIZE}
+              {...register("reply", "all")}
+            />
+            <Radio
+              label={t("feed.filters.reply.following")}
+              size={RADIO_SIZE}
+              {...register("reply", "following")}
+            />
+            <Radio
+              label={t("feed.filters.reply.none")}
+              size={RADIO_SIZE}
+              {...register("reply", "none")}
+            />
+          </RadioGroup>
+          <RadioGroup
+            label={t("post.repost.title")}
+            aria-required
+            orientation="horizontal"
+          >
+            <Radio
+              label={t("feed.filters.repost.all")}
+              size={RADIO_SIZE}
+              {...register("repost", "all")}
+            />
+            <Radio
+              label={t("feed.filters.repost.latest")}
+              size={RADIO_SIZE}
+              {...register("repost", "latest")}
+            />
+            <Radio
+              label={t("feed.filters.repost.none")}
+              size={RADIO_SIZE}
+              {...register("repost", "none")}
+            />
+          </RadioGroup>
         </section>
       </div>
     </>
