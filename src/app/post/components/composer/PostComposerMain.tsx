66b44a925a6bb0bd5dca003899db08a1c3f2f26a
Matsura Yuma <41217985+sabigara@users.noreply.github.com>
2024-01-16 17:20:34 +0900

    Close composer on press esc

--- src/app/post/components/composer/PostComposerMain.tsx
+++ src/app/post/components/composer/PostComposerMain.tsx
@@ -137,7 +137,10 @@ export default function PostComposerMain({
   const handleKeyDown: React.KeyboardEventHandler<HTMLTextAreaElement> = (
     e,
   ) => {
-    console.log(e.key);
+    if (e.key === "Escape") {
+      setOpen(false);
+      return;
+    }
     if (!(isModKey(e.nativeEvent) && e.key === "Enter")) return;
     if (!isPostValid(rt, images.length, !!linkCard.siteMetadata) || isLoading)
       return;
