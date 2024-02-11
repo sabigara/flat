aadab61c58b68be3191292addde352636bf35488
ばいそに / baisony <146979022+baisony@users.noreply.github.com>
2024-02-11 22:15:38 +0900

    Fixed a problem in which the correct parent.root was not specified in replies (#66)
    * Fix issue where parent is specified without specifying the root of the reply target
    
    * add ?
    
    * fix syntax

--- src/app/post/lib/postToReply.ts
+++ src/app/post/lib/postToReply.ts
@@ -1,15 +1,36 @@
-import { AppBskyFeedDefs, AppBskyFeedPost } from "@atproto/api";
+import {
+  AppBskyFeedDefs,
+  AppBskyFeedPost,
+  ComAtprotoRepoStrongRef,
+} from "@atproto/api";
 
-export function postToReply(
-  replyTarget: AppBskyFeedDefs.FeedViewPost,
-): AppBskyFeedPost.ReplyRef {
-  const parent = { cid: replyTarget.post.cid, uri: replyTarget.post.uri };
-  const root =
-    replyTarget.reply && AppBskyFeedDefs.isPostView(replyTarget.reply.root)
-      ? replyTarget.reply.root
-      : parent;
+type FeedViewPost = AppBskyFeedDefs.FeedViewPost;
+type ReplyRef = AppBskyFeedPost.ReplyRef;
+type StrongRef = ComAtprotoRepoStrongRef.Main;
+
+const isPostRecord = AppBskyFeedPost.isRecord;
+const isPostView = AppBskyFeedDefs.isPostView;
+
+export function postToReply(replyTarget: FeedViewPost): ReplyRef {
+  const parent: StrongRef = {
+    cid: replyTarget.post.cid,
+    uri: replyTarget.post.uri,
+  };
+
+  const root: StrongRef = (() => {
+    if (replyTarget.reply && isPostView(replyTarget.reply.root)) {
+      return replyTarget.reply.root;
+    } else if (
+        isPostRecord(replyTarget.post.record) &&
+        replyTarget.post.record.reply
+    ) {
+      return replyTarget.post.record.reply.root;
+    } else {
+      return parent;
+    }
+  })();
+
   return {
-    handle: replyTarget.post.author.handle,
     parent,
     root,
   };
